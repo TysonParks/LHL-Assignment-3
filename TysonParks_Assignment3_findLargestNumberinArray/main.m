367d1534c691a9d9d40c89ddc7ab85d4ab03527a
TysonParks <typarks@gmail.com>
2018-01-28 18:29:56 -0800

    Initialized project

---
+++ TysonParks_Assignment3_findLargestNumberinArray/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TysonParks_Assignment3_findLargestNumberinArray
+//
+//  Created by Tyson Parks on 1/28/18.
+//  Copyright © 2018 Tyson Parks. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
