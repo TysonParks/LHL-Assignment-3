935f8d79b28c08eb93316eebc200835c7360ce71
TysonParks <typarks@gmail.com>
2018-02-05 23:04:47 -0800

    Editted comments and ouput text for clarity.

--- TysonParks_Assignment3_findLargestNumberinArray/main.m
+++ TysonParks_Assignment3_findLargestNumberinArray/main.m
@@ -11,61 +11,66 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         
-        // Initialize array containing numbers of size determined by objects presented
+        // Initialize array containing input list of numbers of size determined by objects presented
         NSArray *numberList = [[NSArray alloc] initWithObjects:@547, @45, @52, @785, @135, @15, @17, @9, @378, @456, @586, @24, @0, @15, @-10, @400, @-30, @52, nil];
         
         // Find and store the object count of this array
         NSInteger numberOfItems = [numberList count];
-        NSLog(@"There are %lu numbers in this list", numberOfItems);
+        NSLog(@"There are %lu numbers in this input list", numberOfItems);
         
-        // Cheeky personification of program
-        NSLog(@"Let's look at these numbers one by one and put them in a new list in order from lowest to highest number");
+        // Lets start crunching numbers!
+        NSLog(@"Let's look at the input list numbers one by one and sort them into an output list in order from lowest to highest");
      
+        
         // Create loop counter i which corresponds to and calls numberList indices
         NSUInteger i = 1;
         // Create loop counter j which corresponds to and calls orderedNumberList indices
         NSUInteger j = 0;
         // Create new mutableArray in which to build new orderedNumberList
         NSMutableArray *orderedNumberList = [[NSMutableArray alloc] initWithObjects:numberList[0], nil];
-        NSLog(@"New list created with the first number of the original list: %@", orderedNumberList);
+        NSLog(@"Output list created with the first number of the input list: %@", orderedNumberList);
 
-        
+        // Sorting algorithm composed of a for loop nested inside another for loop
+        // i represents and calls the index of numbers in the input list
         for (i = 1; i < numberOfItems; i++) {
             NSInteger originalArrayNumberValue = [numberList[i] integerValue];
-            NSLog(@"at i = %lu, originalArrayNumberValue = %li", i, originalArrayNumberValue);
+            NSLog(@"at i = %lu, the input list number = %li", i, originalArrayNumberValue);
+            // j represents and calls the index of numbers in the output list
             for (j = 0; j < i; j++) {
                 NSInteger newArrayNumberValue = [orderedNumberList[j] integerValue];
-                NSLog(@"at j = %lu, newArrayNumberValue = %li", j, newArrayNumberValue);
+                NSLog(@"at j = %lu, the output list number = %li", j, newArrayNumberValue);
                 if (j == (i-1) && originalArrayNumberValue > newArrayNumberValue) {
-                    // The number to be added is larger than all the numbers in the new list
-                    // So add to the new list at after all the other numbers
+                    // The input number to be added is larger than all the numbers in the output list
+                    // So add input number to the output list after all the other numbers
                     NSInteger newIndex = j+1;
                     [orderedNumberList insertObject:numberList[i] atIndex:(newIndex)];
-                    NSLog(@"%li is more than the largest number in the new list, %li so I added %li after %li at index %lu", originalArrayNumberValue, newArrayNumberValue, originalArrayNumberValue, newArrayNumberValue, newIndex);
+                    NSLog(@"%li is larger than the largest number in the output list, %li so I added %li to the output list after %li at index %lu", originalArrayNumberValue, newArrayNumberValue, originalArrayNumberValue, newArrayNumberValue, newIndex);
                     NSUInteger numberOfCurrentItems = [orderedNumberList count];
-                    NSLog(@"The updated list has %lu numbers: %@", numberOfCurrentItems, orderedNumberList);
+                    NSLog(@"The updated output list has %lu numbers: %@", numberOfCurrentItems, orderedNumberList);
                     break;
                 } else if(originalArrayNumberValue <= newArrayNumberValue) {
-                    // The number to be added is smaller than or equal to the current number being checked in the new list
-                    // So add to new list just before the number being checked against
+                    // The input number to be added is smaller than or equal to the current number being checked in the output list
+                    // So add the input number to the output list just before the output number being checked
                     [orderedNumberList insertObject:numberList[i] atIndex:j];
-                    NSLog(@"%li is less than or equal to %li so I added %li before %li at index %lu", originalArrayNumberValue, newArrayNumberValue, originalArrayNumberValue, newArrayNumberValue, j);
+                    NSLog(@"%li is less than or equal to %li so I added %li to the output list before %li at index %lu", originalArrayNumberValue, newArrayNumberValue, originalArrayNumberValue, newArrayNumberValue, j);
                     NSUInteger numberOfCurrentItems = [orderedNumberList count];
-                    NSLog(@"The updated list has %lu numbers: %@", numberOfCurrentItems, orderedNumberList);
+                    NSLog(@"The updated output list has %lu numbers: %@", numberOfCurrentItems, orderedNumberList);
                     break;
                 } else if (originalArrayNumberValue > newArrayNumberValue) {
-                    // The number to be added is larger than the current number being checked in the new list
-                    // So we need to check it against the next number in the new list
+                    // The input number to be added is larger than the current output number being checked in the output list
+                    // So we need to check the input number against the next number in the output list
                     continue;
                 }
                 
             }
             
         }
+        
+        
         // List sorting has completed
-        NSLog(@"Ended sorting at i = %lu and j = %lu", i, j);
+        NSLog(@"Sorting ended at i = %lu and j = %lu", i, j);
         NSUInteger numberOfFinalItems = [orderedNumberList count];
-        NSLog(@"orderedNumberlist now contains %lu items: %@", numberOfFinalItems, orderedNumberList);
+        NSLog(@"The ouput list now contains %lu items: %@", numberOfFinalItems, orderedNumberList);
         
         
         // Then what is the largest number?
@@ -87,7 +92,6 @@ int main(int argc, const char * argv[]) {
             float medianNumberTwoValue = [orderedNumberList[(numberOfFinalItems/2 - 1)] floatValue];
             float medianNumberValue = (medianNumberOneValue + medianNumberTwoValue) / 2;
             medianNumber = [NSNumber numberWithFloat:medianNumberValue];
-
         }
         NSLog(@"The median number in the list is %@", medianNumber);
         
